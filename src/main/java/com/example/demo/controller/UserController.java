b99121ec534d2a72f21ee50b3e28e3d9294b69af
AnPham <anpham1122000@gmail.com>
2024-08-14 23:14:44 +0700

    update-controller

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.dto.request.UserRequestDTO;
 
+import java.util.List;
+
 //@RestController
 //@RequestMapping("/users")
 
@@ -10,8 +12,6 @@ import com.example.demo.dto.request.UserRequestDTO;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-
-
     @PostMapping
     public String addUser(@RequestBody UserRequestDTO userRequestDTO) {
         return "User added";
@@ -23,7 +23,7 @@ public class UserController {
     }
 
     @PatchMapping("/{userId}")
-    public String changeStatus(@PathVariable int userId, @RequestParam(required = false) boolean status) {
+    public String changeStatus(@PathVariable int userId, @RequestParam(required = false, defaultValue = "false") boolean status) {
         System.out.println(status);
         return "User status changed";
     }
@@ -31,5 +31,16 @@ public class UserController {
     @DeleteMapping("/{userId}")
     public String deleteUser(@PathVariable int userId) {
         return "User deleted";
+    }
+
+    @GetMapping("/{userId}")
+    public UserRequestDTO getUser(@PathVariable int userId) {
+        return new UserRequestDTO("An", "ne", "0123456789", "pqan@tma.com.vn");
+    }
+
+    @GetMapping
+    public List<UserRequestDTO> getAllUsers(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int pageSize) {
+        return List.of(new UserRequestDTO("An", "ne", "0123456789", "pqan@tma.com.vn"),
+                new UserRequestDTO("Hoang", "Pham", "0987456321", "pqhoang@tma.com.vn"));
     }
 }
